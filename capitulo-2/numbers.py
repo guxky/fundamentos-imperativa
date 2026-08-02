c3fa2c99edafa0a18657753ca59703f8fdbd5cab
guxky <samuelbangueroortega@gmail.com>
2026-08-01 19:55:14 -0500

    Ejercicios basicos con operaciones de enteros

---
+++ capitulo-2/numbers.py
@@ -0,0 +1,18 @@
+"""
+2-8. Number Eight: Write addition, subtraction, multiplication, and division 
+operations that each result in the number 8 . Be sure to enclose your operations 
+in print statements to see the results . You should create four lines that look 
+like this:
+"""
+print(3+5)
+print(4*2)
+print(10-2)
+print(72/9)
+
+"""
+2-9. Favorite Number: Store your favorite number in a variable . Then, using 
+that variable, create a message that reveals your favorite number . Print that 
+message 
+"""
+number = 4
+print("my favorite number is: "+ str(number))
